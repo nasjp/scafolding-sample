802ab56d891f12edd964f3fb57039b96f431a73d
nas <nas.silver.bullet@gmail.com>
2020-03-19 01:20:01 +0900

    :+1: remove unnssary func

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"math"
 	"os"
 	"strings"
-
-	"github.com/ChimeraCoder/gojson"
 )
 
 func main() {
@@ -21,36 +17,6 @@ func main() {
 }
 
 func run() error {
-	// return part1()
-	return part2()
-}
-
-func part1() error {
-	f, err := os.Open("sample.json")
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	b, err := gojson.Generate(f, gojson.ParseJson, strings.Split(f.Name(), ".")[0], "_", []string{"json"}, true, true)
-	if err != nil {
-		return err
-	}
-	sc := bufio.NewScanner(bytes.NewBuffer(b))
-	for sc.Scan() {
-		if sc.Text() == "" {
-			continue
-		}
-		if strings.HasPrefix(sc.Text(), "package") {
-			continue
-		}
-		fmt.Println(sc.Text())
-	}
-	return nil
-}
-
-func part2() error {
-	// f, err := os.OpenFile("sample.json", os.O_RDWR, 0664)
 	f, err := os.Open("sample.json")
 	if err != nil {
 		return err
