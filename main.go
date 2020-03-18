4c74f25318f8635462196c82bda87cd5e29d9115
nas <nas.silver.bullet@gmail.com>
2020-03-19 01:18:51 +0900

    :+1: no indent same line

--- main.go
+++ main.go
@@ -61,40 +61,38 @@ func part2() error {
 		return err
 	}
 
-	s := parser(m, 0)
+	s := parser(m, 0, false, false)
 
 	fmt.Println(s)
 	return nil
 }
 
-var indentStr = "  "
-
-func parser(i interface{}, nest int) string {
+func parser(i interface{}, nest int, isSameLine bool, isLastElement bool) string {
 	switch i.(type) {
 	case string:
-		return fmt.Sprintf("%s\"%s\",\n", strings.Repeat(indentStr, nest), i.(string))
+		return fmt.Sprintf("%s\"%s\"%s\n", getSpaces(nest, isSameLine), i.(string), getComma(isLastElement))
 	case float64:
-		return fmt.Sprintf("%s%s,\n", strings.Repeat(indentStr, nest), numberPerser(i.(float64)))
+		return fmt.Sprintf("%s%s%s\n", getSpaces(nest, isSameLine), numberPerser(i.(float64)), getComma(isLastElement))
 	case bool:
-		return fmt.Sprintf("%s%t,\n", strings.Repeat(indentStr, nest), i.(bool))
+		return fmt.Sprintf("%s%t%s\n", getSpaces(nest, isSameLine), i.(bool), getComma(isLastElement))
 	case nil:
-		return fmt.Sprintf("%s%s,\n", strings.Repeat(indentStr, nest), "<nil>")
+		return fmt.Sprintf("%s%s%s\n", getSpaces(nest, isSameLine), "<nil>", getComma(isLastElement))
 	case []interface{}:
+		txt := fmt.Sprintf("%s%s\n", getSpaces(nest, isSameLine), "[")
 		s := i.([]interface{})
-		txt := fmt.Sprintf("%s%s\n", strings.Repeat(indentStr, nest), "[")
 		for _, v := range s {
-			txt += parser(v, nest+1)
+			txt += parser(v, nest+1, false, false)
 		}
-		txt += fmt.Sprintf("%s%s,\n", strings.Repeat(indentStr, nest), "]")
+		txt += fmt.Sprintf("%s%s%s\n", getSpaces(nest, false), "]", getComma(isLastElement))
 		return txt
 	case map[string]interface{}:
+		txt := fmt.Sprintf("%s%s\n", getSpaces(nest, isSameLine), "{")
 		m := i.(map[string]interface{})
-		txt := fmt.Sprintf("%s%s\n", strings.Repeat(indentStr, nest), "{")
 		for k, v := range m {
-			txt += fmt.Sprintf("%s%s: ", strings.Repeat(indentStr, nest+1), k)
-			txt += parser(v, nest+1)
+			txt += fmt.Sprintf("%s%s:", getSpaces(nest+1, false), k)
+			txt += parser(v, nest+1, true, false)
 		}
-		txt += fmt.Sprintf("%s%s,\n", strings.Repeat(indentStr, nest), "}")
+		txt += fmt.Sprintf("%s%s%s\n", getSpaces(nest, false), "}", getComma(isLastElement))
 		return txt
 	}
 	return ""
@@ -107,4 +105,17 @@ func numberPerser(f float64) string {
 	return fmt.Sprintf("%f", f)
 }
 
-// func getSpace()
+func getSpaces(nest int, isSameLine bool) string {
+	spaces := strings.Repeat("  ", nest)
+	if isSameLine {
+		spaces = " "
+	}
+	return spaces
+}
+
+func getComma(isLastElement bool) string {
+	if isLastElement {
+		return ""
+	}
+	return ","
+}
